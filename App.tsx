c3737977afc89601b6c3f70ee1a829205798c6c5
Ravi kiran <dev@tripfriday.com>
2024-10-01 18:57:53 +0530

    flight screen added

--- App.tsx
+++ App.tsx
@@ -1,12 +1,16 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 import FlightSearch from './components/Flight/FlightSearch'
+import { store } from './components/redux/store'
+import { Provider } from 'react-redux'
 
 const App = () => {
   return (
-    <View style={{flex:1}}>
+    <Provider store={store}>
+      <View style={{flex:1}}>
      <FlightSearch/>
     </View>
+    </Provider>
   )
 }
 
